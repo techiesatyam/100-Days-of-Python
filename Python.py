7ae36c3cf5c4ebcdb579ff3dbfa496d15dbeeadc
Om Mishra <mishraom0805@gmail.com>
2026-07-08 12:31:45 +0530

    Removing minor error from the program

--- Python.py
+++ Python.py
@@ -1,6 +1,13 @@
 # This program asks user for name and greets them.
 
 name = input("What's your name? ")
+
+# Remove white space from the str.
+name = name.strip()
+
+#Capitalize the first letter of the name.
+name = name.capitalize()
+
 print(f"Hello, {name}!")
 
 # Using escape characters to include quotes in the output
