c3e0f153940e2a3d47144754a7e81eef62ccd7cb
Om Mishra <mishraom0805@gmail.com>
2026-07-09 12:10:09 +0530

    Addition using the integer, float and round off function

--- Python.py
+++ Python.py
@@ -5,16 +5,21 @@ name = input("What's your name? ")
 # Remove white space from the str.
 name = name.strip().title()
 
-# Split the name into first and last name
-first, last = name.split(" ")
-
-print(f"Hello, {first}")
+print(f"Hello, {name}")
 
 # Using escape characters to include quotes in the output
 print("Hello, \"friend\"")
 
-# Calculator 
-x = int(input ("Enter a number: "))
-y = int(input ("Enter a number: "))
+#Arithmatic operations
+
+#Addition Operator using int operator
+x = int(input("Enter the number: "))
+y = int(input("Enter the number: "))
 
 print(x + y)
+
+# Addition Operator using float and round off operator
+x = float(input("Enter number: "))
+y = float(input("Enter number: "))
+
+print(round(x + y))
