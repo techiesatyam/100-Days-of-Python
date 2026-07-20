3d976abcb005d1c002ea8c33ee9f87b473b34501
Om Mishra <mishraom0805@gmail.com>
2026-07-20 20:31:13 +0530

    Calculating the age of user by defining own function

---
+++ Age_calculator.py
@@ -0,0 +1,16 @@
+# Asking user for their name by creating a function.
+def cal(YOB, Year):
+    print(f"This is coming from below cal {YOB}")
+    name = input("What's your good name? ")
+    name = name.strip().title()
+    print(f"Hello , {name}. How are you doing?")
+
+# Calculating user age in year they want.
+    Age = Year - YOB
+
+    print(f"Your age in {Year} year will be {Age} years old.")
+
+
+print("This is line 15")
+cal(Year = int(input("What's the year you want to know your age? ")), YOB = int(input("What's your birth year? ")))
+cal(int(input("What's your birth year? ")), int(input("What's the year you want to know your age? ")))
