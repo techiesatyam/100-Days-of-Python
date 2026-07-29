69d724c77ea0729ed9b2e6af1e569548202aade7
Satyam Mishra <116354713+techiesatyam@users.noreply.github.com>
2026-07-29 22:30:33 +0530

    Delete Deep.py

--- Deep.py
+++
@@ -1,8 +0,0 @@
-def Avishi():
-    answer = input("What is the answer to Great question of life, the Universe, and Everything? ")
-    if answer == "42" or answer == "forty-two" or answer == "forty two":
-        return("Yes")
-    else:
-        return ("No")
-
-print(Avishi())
