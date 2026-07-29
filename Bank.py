210c472e152528ac43f6d0dcd7f5f3dccb649f38
Om Mishra <mishraom0805@gmail.com>
2026-07-29 17:04:20 +0530

    Using "match" keyword instead of "if", "elif" conditiionals

--- Bank.py
+++ Bank.py
@@ -1,13 +1,14 @@
 # Defining a function for greeting .
-def Main():
-    Formal = input("Greeting: ")
-    if Formal == "Hello" or Formal == "Hello, Newman":
-        return("$0")
-    elif Formal == "Hey" or Formal == "How you doing?" or Formal == "How's it going?":
-        return("$20")
-    elif Formal == "What's happening?" or Formal == "What's up?":
-        return("$100")
-    else:
-        return("Sorry!!, Invalid greeting")
-               
-print(Main())
+def Main(Formal):
+     
+    match Formal:
+        case "Hello" | "Hello, Newman":
+            return("$0")
+        case "Hey" | "How are you doing?" | "How's it going?":
+            return("$20")
+        case "What's happening?" | "What's up?":
+            return("$100")
+        case _:
+            return("Sorry! we didn't get you")        
+       
+print(Main(input("Greeting: ")))
