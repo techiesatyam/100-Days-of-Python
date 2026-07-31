8a8c2a6d9a407a96a108e8ef157e2ef6e9690358
Om Mishra <mishraom0805@gmail.com>
2026-07-31 19:25:54 +0530

    Using "for" loop and using combinnaton both "for" and "while" loop

--- Chapter - 3 Loops/Loop.py
+++ Chapter - 3 Loops/Loop.py
@@ -5,3 +5,21 @@ def Call():
         print("Bye")
         O = O + 1
 Call()
+
+# Using for loop
+for i in [0, 1, 2]:
+    print(i)
+
+#Using while and for loop together
+def UIX():
+    while True:
+        y = int(input("What's y? "))
+        if y < 0:
+            continue
+        else:
+            break
+
+    for _ in range(y):
+        print(f"Goodbye! {y}")
+
+UIX()
