7fc2acfcf9f901c5d2abcbd96dd806091996dba4
Om Mishra <mishraom0805@gmail.com>
2026-08-03 21:36:57 +0530

    Buliding two-dimension blocks of Mario game using "for" loop

--- Chapter - 3 Loops/Loop.py
+++ Chapter - 3 Loops/Loop.py
@@ -48,3 +48,16 @@ vegetables = [
 ]
 for v in vegetables:
     print(v["name"], v["type"], sep=": ", end="\n")
+
+# Mario game using "for" loop
+def Mario():
+    print_column(3)
+    print_row(4)
+
+def print_column(height):
+    for _ in range(height):
+        print("#", end="")
+
+def print_row(width):
+        print("?" * width, end="\n")
+Mario()
