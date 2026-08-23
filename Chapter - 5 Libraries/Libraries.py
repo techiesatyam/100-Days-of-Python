b5e6c3b4809a012beb9a2d4768e6b430cefe2539
Om Mishra <mishraom0805@gmail.com>
2026-08-23 22:02:06 +0530

    Using "random" module and some of its function

---
+++ Chapter - 5 Libraries/Libraries.py
@@ -0,0 +1,15 @@
+import random 
+
+# Function for choosing 
+choose = random.choice(["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"])
+print(choose)
+
+# Function for selecting number randomly
+choice = random.randint(1, 5)
+print(choice)
+
+#Function for shuffling the 
+rome = ["!", "@", "#", "$", "^", "<>"]
+random.shuffle(rome)
+for ro in rome:
+    print(ro)
