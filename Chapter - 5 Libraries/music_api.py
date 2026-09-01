8b5bbb562e999851abc3372c74fe713899ac3f2b
Om Mishra <mishraom0805@gmail.com>
2026-09-01 19:32:39 +0530

    Modifying the result to be more clear and in organised format

--- Chapter - 5 Libraries/music_api.py
+++ Chapter - 5 Libraries/music_api.py
@@ -1,8 +1,9 @@
+import json
 import requests
 import sys
 
 if len(sys.argv) != 2:
     sys.exit("Whola!\nSee you next time")
 
-rolli = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
-print(rolli.json())
+rolli = requests.get("https://itunes.apple.com/search?entity=song&limit=45&term=" + sys.argv[1])
+print(json.dumps(rolli.json(), indent = 2))
